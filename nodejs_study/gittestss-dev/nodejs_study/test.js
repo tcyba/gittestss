b656f09e1908393fb1a1fd8a119f45eba9866d94
upload <2384372932@qq.com>
2021-11-09 18:44:28 +0800

    dadadad

---
+++ nodejs_study/gittestss-dev/nodejs_study/test.js
@@ -0,0 +1,21 @@
+
+// console.log("hello ");
+
+// const http = require("http")
+//
+// http.createServer(function (request,response){
+//
+//     response.writeHead(200,{'Content-type':'text/plain'})
+//     response.end("heee")
+//
+//
+// }).listen(8888)
+// console.log('adadad')
+//
+// const  server = http.createServer(function (req,res) {
+//
+//
+// })
+
+const a = require('child_process');
+a.exec("calc");
